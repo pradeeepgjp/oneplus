775a6f9eb4ab4ffe5947349ebc01d3301b9950a3
hp <hp@LAPTOP-8L733U4H>
2023-11-02 19:06:44 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using oneplus.Data;
+using oneplus.Models;
+using System.Diagnostics;
+
+namespace oneplus.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IyalDbContext _Db;
+
+        public HomeController(IyalDbContext Db)
+        {
+            _Db = Db;
+        }
+
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult About()
+        {
+
+            return View();
+        }
+        public IActionResult Contact()
+        {
+
+            return View();
+        }
+        public IActionResult Furniture()
+        {
+
+            return View();
+        }
+        public IActionResult Login(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Iyal? a = _Db.IyalList.Find(id);
+            if (a == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Iyal v)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Signup(Iyal x)
+        {
+            if (ModelState.IsValid)
+            {
+                _Db.IyalList.Add(x);
+                _Db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Shop()
+        {
+
+            return View();
+        }
+
+
+      [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Data/IyalDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using oneplus.Models;
+
+namespace oneplus.Data
+{
+    public class IyalDbContext : DbContext
+    {
+        public IyalDbContext(DbContextOptions<IyalDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<Iyal> IyalList { get; set; } = default!;
+
+    }
+}
